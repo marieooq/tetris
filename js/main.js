503254b6ecf31906f4a5b640b49311330421925a
Marie Otaki <otakimarie@gmail.com>
2019-09-10 17:47:00 -0700

    modified files

---
+++ js/main.js
@@ -0,0 +1,49 @@
+let array = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+const draw = () => {
+  let trs = document.querySelectorAll("tr");
+
+  for (let i = 0; i < trs.length; i++) {
+    let trNum = trs[i];
+    for (let j = 0; j < trNum.children.length; j++) {
+      trNum.children[j].classList.remove("stick");
+
+      switch (array[i][j]) {
+        case 1:
+          trNum.children[j].classList.add("stick");
+          break;
+        default:
+          trNum.children[j].classList.add("default");
+      }
+    }
+  }
+
+  const fall = () => {
+    //an array that checks if there is a block under
+    let under = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  };
+  // setInterval(() => {
+  //   draw();
+  // }, 500);
+};
