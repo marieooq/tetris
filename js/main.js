becb221bc8fa120700945aec8dface8197c16ff6
Marie Otaki <otakimarie@gmail.com>
2019-09-10 18:12:19 -0700

    add fall function

--- js/main.js
+++ js/main.js
@@ -42,8 +42,34 @@ const draw = () => {
   const fall = () => {
     //an array that checks if there is a block under
     let under = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+    for (let i = 19; i >= 0; i--) {
+      for (let j = 0; j < 10; j++) {
+        if (under[j] == 0) {
+          if (array[i][j] == 0) {
+            //neither there are anything under this array nor it's a block
+            under[j] = 0;
+          } else {
+            //there is nothing under this array and it's a block
+            array[i + 1][j] = array[i][j];
+            array[i][j] = 0;
+            under[j] = 0;
+          }
+        } else {
+          if (array[i][j] == 0) {
+            //A block is under this array and itself is NOT a block
+            under[j] = 0;
+          } else {
+            //A block is under this array and itself is a block
+            under[j] = 1;
+          }
+        }
+      }
+    }
   };
-  // setInterval(() => {
-  //   draw();
-  // }, 500);
+
+  setInterval(() => {
+    fall();
+    draw();
+  }, 500);
 };
