4e1e781b944231ea646b424275746f02e3a41038
Marie Otaki <otakimarie@gmail.com>
2019-09-12 16:43:24 -0700

    refactor the previous cords

--- js/main.js
+++ js/main.js
@@ -1,232 +1,287 @@
-//check if there is a block that can be moved
-let moveFlag = 0;
+class PlayArea {
+  draw() {
+    let trs = document.querySelectorAll("tr");
 
-let resetFlag = 0;
+    for (let i = 0; i < trs.length; i++) {
+      let trNum = trs[i];
+      for (let j = 0; j < trNum.children.length; j++) {
+        trNum.children[j].classList.remove("stick");
 
-let array = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-//show current position of the blocks
-let move = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-const draw = () => {
-  let trs = document.querySelectorAll("tr");
-
-  for (let i = 0; i < trs.length; i++) {
-    let trNum = trs[i];
-    for (let j = 0; j < trNum.children.length; j++) {
-      trNum.children[j].classList.remove("stick");
-
-      switch (array[i][j]) {
-        case 1:
-          trNum.children[j].classList.add("stick");
-          break;
-        default:
-          trNum.children[j].classList.add("default");
+        switch (this.array[i][j]) {
+          case 1:
+            trNum.children[j].classList.add("stick");
+            break;
+          default:
+            trNum.children[j].classList.add("default");
+        }
       }
     }
   }
 
-  const fall = () => {
+  fall() {
     //an array that checks if there is a block under
     let under = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
     for (let i = 19; i >= 0; i--) {
       for (let j = 0; j < 10; j++) {
-        if (resetFlag == 1) {
-          return false;
-        } else if (under[j] == 0) {
-          if (array[i][j] == 0) {
+        if (under[j] == 0) {
+          if (this.array[i][j] == 0) {
             //neither there are anything under this array nor it's a block
             under[j] = 0;
           } else {
             //there is nothing under this array and it's a block
-            array[i + 1][j] = array[i][j];
-            array[i][j] = 0;
+            this.array[i + 1][j] = this.array[i][j];
+            this.array[i][j] = 0;
 
             //modify the move array
-            if (move[i][j] == 1) {
-              move[i][j] = 0;
-              move[i + 1][j] = 1;
+            if (this.move[i][j] == 1) {
+              this.move[i][j] = 0;
+              this.move[i + 1][j] = 1;
             }
             under[j] = 0;
           }
         } else {
-          if (array[i][j] == 0) {
+          if (this.array[i][j] == 0) {
             //A block is under this array and itself is NOT a block
             under[j] = 0;
           } else {
             //A block is under this array and itself is a block
-            if (move[i][j] == 1) {
-              console.log(`i:${i}`);
-              console.log(`j:${j}`);
-              console.log("array");
-              console.log(array);
-              console.log("move");
-              console.log(move);
-              resetMove();
-              console.log("move after resetMove was excuted");
-              console.log(move);
-
-              resetFlag = 1;
-            }
+            // if (move[i][j] == 1) {
+            //   if (i == 19) {
+            //     fixFlag = 1;
+            //     fixArray = array.concat();
+            //     console.log(fixArray);
+            //     return false;
+            //   }
+            // }
             under[j] = 1;
           }
         }
       }
     }
-  };
+  }
 
-  const resetMove = () => {
+  resetMove() {
     moveFlag = 0;
     for (let i = 0; i < 20; i++) {
       for (let j = 0; j < 10; j++) {
         move[i][j] = 0;
       }
     }
-  };
+  }
 
-  const genBlock = blockNum => {
-    if (moveFlag == 0) {
+  genBlock(blockNum) {
+    if (this.moveFlag == 0) {
       switch (blockNum) {
         case 1:
-          array[0][5] = 1;
-          array[1][5] = 1;
-          array[2][5] = 1;
-          array[3][5] = 1;
+          this.array[0][5] = 1;
+          this.array[1][5] = 1;
+          this.array[2][5] = 1;
+          this.array[3][5] = 1;
 
-          //modifiy the move array
-          move[0][5] = 1;
-          move[1][5] = 1;
-          move[2][5] = 1;
-          move[3][5] = 1;
+          //modifiy the move this.array
+          this.move[0][5] = 1;
+          this.move[1][5] = 1;
+          this.move[2][5] = 1;
+          this.move[3][5] = 1;
 
           break;
 
         case 2:
-          array[0][1] = 1;
-          array[0][2] = 1;
-          array[1][0] = 1;
-          array[1][1] = 1;
+          this.array[0][1] = 1;
+          this.array[0][2] = 1;
+          this.array[1][0] = 1;
+          this.array[1][1] = 1;
 
           //modifiy the move array
-          move[0][1] = 1;
-          move[0][2] = 1;
-          move[1][0] = 1;
-          move[1][1] = 1;
+          this.move[0][1] = 1;
+          this.move[0][2] = 1;
+          this.move[1][0] = 1;
+          this.move[1][1] = 1;
 
           break;
       }
-      moveFlag = 1;
+      this.moveFlag = 1;
     }
-  };
+  }
 
-  const moveBlockRight = () => {
+  moveBlockRight() {
     for (let i = 19; i >= 0; i--) {
-      let newMove = move[i].concat();
+      let newMove = this.move[i].concat();
       for (let j = 8; j >= 0; j--) {
-        if (move[i][j] == 1) {
-          array[i][j + 1] = array[i][j];
-          array[i][j] = 0;
+        if (this.move[i][j] == 1) {
+          this.array[i][j + 1] = this.array[i][j];
+          this.array[i][j] = 0;
           newMove[j + 1] = 1;
           newMove[j] = 0;
         }
       }
-      move[i] = newMove;
+      this.move[i] = newMove;
     }
-  };
+  }
 
-  const moveBlockLeft = () => {
+  moveBlockLeft() {
     for (let i = 19; i >= 0; i--) {
-      let newMove = move[i].concat();
+      let newMove = this.move[i].concat();
       for (let j = 1; j < 10; j++) {
-        if (move[i][j] == 1) {
-          array[i][j - 1] = array[i][j];
-          array[i][j] = 0;
+        if (this.move[i][j] == 1) {
+          this.array[i][j - 1] = this.array[i][j];
+          this.array[i][j] = 0;
           newMove[j - 1] = 1;
           newMove[j] = 0;
         }
       }
-      move[i] = newMove;
+      this.move[i] = newMove;
     }
-  };
+  }
 
-  const checkDelete = () => {
+  checkDelete() {
     for (let i = 19; i >= 0; i--) {
-      if (!array[i].includes(0)) {
+      if (!this.array[i].includes(0)) {
         for (let j = 0; j < 10; j++) {
-          array[i][j] = 0;
+          this.array[i][j] = 0;
         }
       }
     }
-    moveFlag = 0;
-  };
+    this.moveFlag = 0;
+  }
 
-  document.onkeydown = e => {
-    switch (e.code) {
-      case "Space":
-        resetFlag = 0;
-        genBlock(2);
-        break;
+  constructor() {
+    this.moveFlag = 0;
+    this.fixFlag = 0;
+    this.array = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
 
-      case "ArrowRight":
-        moveBlockRight();
-        break;
+    this.move = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
 
-      case "ArrowLeft":
-        moveBlockLeft();
-        break;
-    }
-    // draw();
-    iteration();
-  };
+    this.fixArray = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+  }
+}
 
-  const iteration = () => {
-    setInterval(() => {
-      checkDelete();
-      fall();
-      draw();
-    }, 500);
-  };
-};
+class Game {
+  draw() {
+    this.playArea.draw();
+  }
+
+  fall() {
+    this.playArea.fall();
+  }
+
+  resetMove() {
+    this.playArea.resetMove();
+  }
+
+  genBlock(blockNum) {
+    this.playArea.genBlock(blockNum);
+  }
+
+  moveBlockRight() {
+    this.playArea.moveBlockRight();
+  }
+
+  moveBlockLeft() {
+    this.playArea.moveBlockLeft();
+  }
+
+  checkDelete() {
+    this.playArea.checkDelete();
+  }
+
+  constructor() {
+    this.playArea = new PlayArea();
+
+    document.onkeydown = e => {
+      switch (e.code) {
+        case "Space":
+          this.genBlock(2);
+          break;
+
+        case "ArrowRight":
+          this.moveBlockRight();
+          break;
+
+        case "ArrowLeft":
+          this.moveBlockLeft();
+          break;
+      }
+      // draw();
+      iteration();
+    };
+
+    const iteration = () => {
+      setInterval(() => {
+        this.checkDelete();
+        this.fall();
+        this.draw();
+      }, 500);
+    };
+  }
+}
+
+var game = "";
+
+function startGame() {
+  game = new Game();
+}
---
+++ js/tetris.js
@@ -0,0 +1,162 @@
+class ShapeA {
+  constructor(initialRotation, posX, posY) {
+    this.data = [0, 0, 0, 0];
+    this.data[0] = [0, 0, 0, 0];
+    this.data[1] = [0, 0, 0, 0];
+    this.data[2] = [0, 0, 0, 0];
+    this.data[3] = [1, 1, 1, 1];
+  }
+}
+
+class ShapeB {
+  constructor(initialRotation, posX, posY) {
+    this.data = [0, 0, 0, 0];
+    this.data[0] = [0, 0, 0, 0];
+    this.data[1] = [0, 0, 0, 0];
+    this.data[2] = [0, 0, 0, 0];
+    this.data[3] = [1, 1, 1, 1];
+  }
+}
+
+class ShapeC {
+  constructor(initialRotation, posX, posY) {
+    this.data = [0, 0, 0, 0];
+    this.data[0] = [0, 0, 0, 0];
+    this.data[1] = [0, 0, 0, 0];
+    this.data[2] = [0, 0, 0, 0];
+    this.data[3] = [1, 1, 1, 1];
+  }
+}
+
+class ShapeD {
+  constructor(initialRotation, posX, posY) {
+    this.data = [0, 0, 0, 0];
+    this.data[0] = [0, 0, 0, 0];
+    this.data[1] = [0, 0, 0, 0];
+    this.data[2] = [0, 0, 0, 0];
+    this.data[3] = [1, 1, 1, 1];
+  }
+}
+
+class ShapeE {
+  constructor(initialRotation, posX, posY) {
+    this.data = [0, 0, 0, 0];
+    this.data[0] = [0, 0, 0, 0];
+    this.data[1] = [0, 0, 0, 0];
+    this.data[2] = [0, 0, 0, 0];
+    this.data[3] = [1, 1, 1, 1];
+  }
+}
+
+class ShapeF {
+  constructor(initialRotation, posX, posY) {
+    this.data = [0, 0, 0, 0];
+    this.data[0] = [0, 0, 0, 0];
+    this.data[1] = [0, 0, 0, 0];
+    this.data[2] = [0, 0, 0, 0];
+    this.data[3] = [1, 1, 1, 1];
+  }
+}
+
+class ShapeG {
+  constructor(initialRotation, posX, posY) {
+    this.data = [0, 0, 0, 0];
+    this.data[0] = [0, 0, 0, 0];
+    this.data[1] = [0, 0, 0, 0];
+    this.data[2] = [0, 0, 0, 0];
+    this.data[3] = [1, 1, 1, 1];
+  }
+}
+
+class Shape {
+  constructor(
+    whichShape = 0,
+    initialRotation = 0,
+    initialPosX = 0,
+    initialPosY = 0
+  ) {
+    this.shape = "";
+
+    if (whichShape === 0) {
+      this.shape = new ShapeA(initialRotation, initialPosX, initialPosY);
+    } else if (whichShape === 1) {
+      this.shape = new ShapeB(initialRotation, initialPosX, initialPosY);
+    } else if (whichShape === 2) {
+      this.shape = new ShapeC(initialRotation, initialPosX, initialPosY);
+    } else if (whichShape === 3) {
+      this.shape = new ShapeD(initialRotation, initialPosX, initialPosY);
+    } else if (whichShape === 4) {
+      this.shape = new ShapeE(initialRotation, initialPosX, initialPosY);
+    } else if (whichShape === 5) {
+      this.shape = new ShapeF(initialRotation, initialPosX, initialPosY);
+    } else if (whichShape === 6) {
+      this.shape = new ShapeG(initialRotation, initialPosX, initialPosY);
+    }
+  }
+
+  rotateShape(toPosition = 0) {
+    this.shape.rotateShape(toPosition);
+  }
+
+  moveShape(x, y) {}
+}
+
+class PlayArea {
+  constructor() {
+    this.shapes = [""];
+
+    this.data = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    drawPlayArea();
+    createRandomShape;
+  }
+
+  createRandomShape() {
+    this.shapes[0] = new Shape(0, 0, 0, 5);
+    this.shapes[1] = new Shape(0, 0, 0, 5);
+    this.shapes[0] = new Shape(0, 0, 0, 5);
+  }
+
+  drawShape(x, y) {
+    this.shapes[0].data = new Shape(0, 0, 0, 5);
+
+    var aShape = this.shapes[0];
+
+    for (var i = 0; i < aShape.data[0].length; i++) {
+      for (var j = 0; j < aShape.data[i].length; j++) {
+        if (aShape.data[i][j] === 1) {
+          this.data[i + initialPosX][j + initialPosY] = aShape.data[i][j];
+          //transfer to playerarea
+        }
+      }
+    }
+  }
+}
+
+class Game {
+  constructor() {
+    PlayArea = new PlayArea();
+  }
+}
+折りたたむ;
